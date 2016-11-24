3926c59d870d374070404d05d93d222ed62b6ab2
Andrew Teologov <teologov.and@gmail.com>
2016-11-24 09:06:09 +0200

    added logger, and dependencies

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * Teo.JS logger
+ * @author Andrew Teologov <teologov.and@gmail.com>
+ * @date 11/24/16
+ */
+
+const logger = require("./lib/logger");
+
+"use strict";
+
+module.exports = logger;
+
