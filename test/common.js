5b632e1ddb939dc839181313ab2e23061d6c2db8
Andrew Teologov <teologov.and@gmail.com>
2016-11-24 09:12:11 +0200

    tests

---
+++ test/common.js
@@ -0,0 +1,12 @@
+/*!
+ * Common
+ * @author Andrew Teologov <teologov.and@gmail.com>
+ * @date 11/16/16
+ */
+
+"use strict";
+
+global.sinon = require("sinon");
+global.chai = require("chai");
+global.expect = global.chai.expect;
+global.assert = global.chai.assert;
